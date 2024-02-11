8141d203721c2296d5036a8aac6d87cd2b7d72c1
Anil Patil <anilrspatil1992@gmail.com>
2024-02-11 12:29:04 +0530

    add files to new repo

---
+++ src/main/java/com/nil/WarDeploymentByJenkins/WarDeploymentByJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.nil.WarDeploymentByJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarDeploymentByJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarDeploymentByJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nil/WarDeploymentByJenkins/WarDeploymentByJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nil.WarDeploymentByJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarDeploymentByJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
